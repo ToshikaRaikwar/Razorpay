3c3c3b580e39fd6e846cb10e920fda42c7db6886
Toshika Raikwar <raikwartoshika@gmail.com>
2023-06-24 20:59:17 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [page, setPage] = useState({
+    options: [
+      {
+        name: 'Free',
+        desc: '1 Question per day',
+        price: 0,
+      },
+      {
+        name: 'Silver',
+        desc: '5 Questions per day',
+        price: 100,
+      },
+      {
+        name: 'Premium',
+        desc: 'Unlimited Questions per day',
+        price: 1000,
+      },
+    ],
+  });
+
+  const initPayment = async (data) => {
+    const options = {
+      key: 'rzp_test_RHkbnq3KbCLC6C',
+      amount: data.amount.toString(),
+      currency: data.currency,
+      name: 'Offers', // Replace with your product name
+      order_id: data.id,
+      handler: async (response) => {
+        try {
+          const verifyUrl = 'http://localhost:8080/api/payment/verify';
+          const { data } = await axios.post(verifyUrl, response);
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      theme: {
+        color: '#3399cc',
+      },
+    };
+  
+    const rzp = new window.Razorpay(options);
+    rzp.on('payment.failed', function (response) {
+      console.log(response.error.description);
+    });
+    rzp.open();
+  };
+  
+  const handlePayment = async (price) => {
+    if (price === 0) {
+      console.log("Free option selected. No payment required.");
+      alert("Free option selected. No payment required.")
+      return;
+    }
+    try {
+      const orderUrl = 'http://localhost:8080/api/payment/orders';
+      const { data } = await axios.post(orderUrl, { amount: price });
+      console.log(data);
+      initPayment(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="page-container">
+        {page.options.map((option, index) => (
+          <div key={index} className='page'>
+            <p className="page_name">{option.name}</p>
+            <p className="page_desc">{option.desc}</p>
+            <p className="page_price">
+              Price: <span>&#8377;{option.price}</span>
+            </p>
+            <button className="buy_btn" onClick={() => handlePayment(option.price)}>
+              Buy Now
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
